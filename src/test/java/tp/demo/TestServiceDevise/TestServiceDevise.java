192b73c3f0450a46db6a395501381b165d367400
Dabs-Kev <tsankera@net-c.com>
2024-09-11 14:53:06 +0200

    ecriture code testservice.java

--- src/test/java/tp/demo/TestServiceDevise/TestServiceDevise.java
+++ src/test/java/tp/demo/TestServiceDevise/TestServiceDevise.java
@@ -1,5 +1,50 @@
 package tp.demo.TestServiceDevise;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import tp.demo.model.Devise;
+import tp.demo.repository.DeviseRepository;
+import tp.demo.service.ServiceDevise;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("h2")
 public class TestServiceDevise {
+
+    @Autowired
+    private ServiceDevise serviceDevise; // à tester
+
+    @Autowired
+    private DeviseRepository deviseRepository;
+
+    public void initData(){
+        deviseRepository.save(new Devise("EUR", "Euro", 1.0));
+        deviseRepository.save(new Devise("USD", "Dollar", 1.1));
+        deviseRepository.save(new Devise("GBP", "Livre", 1.2));
+    }
+
+        /*
+    @BeforeEach
+    public void init(){
+        serviceDevise=new ServiceDevise(); //V1 en court circuitant Spring
+        //la V2 avec @springBootTest et @Autowired est mieux en Spring
+    }
+    */
+
+    @Test
+    public void testFindAllDevise() {
+        initData();
+        List<Devise> liste = serviceDevise.findAllDevises();
+        //idéalement logger.debug(...)
+        System.out.println("liste=" + liste); // aide au debug
+        assertTrue(liste.size()>=2,
+                "la taille de la liste devrait être >=2");
+    }
 
 }
